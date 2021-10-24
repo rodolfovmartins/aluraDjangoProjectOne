d7c146f94b595af2693824230c21e3f14c79f728
Rodolfo Martins <rodolfovmartins@gmail.com>
2021-10-23 22:43:18 -0300

    Removing DS_Store

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -12,15 +12,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-na87w0ro&8(a5wzoq&qp8rv@qj=g*^yv%iaa0*cg@k3_()0aq)'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
